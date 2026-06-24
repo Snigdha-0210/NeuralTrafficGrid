0d75b3732dfcc138ff4fa35364a55ba469a3458b
Snigdha Gorai <snigdha.24bce7185@vitapstudent.ac.in>
2026-06-25 00:40:00 +0530

    chore: add deployment configurations

--- lib/api.ts
+++ lib/api.ts
@@ -32,7 +32,7 @@ export type PSOOptimizationResult = {
   }
 }
 
-const API_BASE = 'http://localhost:8000/api'
+const API_BASE = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000/api'
 
 export type PredictionRequest = {
   north_count: number
